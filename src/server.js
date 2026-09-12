9fdd977dcb6e70f5f415128c9efc496cb5492047
Aditya B D <lalitaadityadharaneppanavar@gmail.com>
2026-09-13 01:59:15 +0530

    test github webhook

--- src/server.js
+++ src/server.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 3000;
 app.get("/", (req, res) => {
     res.json({
         message: "Hello from CI/CD",
-        version: "1.0.1"
+        version: "1.0.2"
     });
 });
 
